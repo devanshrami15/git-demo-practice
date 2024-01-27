8aad0882278f5565e6c944ec023b2c9786460bca
devanshrami15 <147177225+devanshrami15@users.noreply.github.com>
2024-01-27 15:09:24 +0000

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("My name is Devansh");
+        System.out.println("My name is Devansh, How are you?");
     }
 }
